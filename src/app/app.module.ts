e3d140ae032af437405e01cabd65c9a26cff7eec
RoWEN-FCUB <carlos@ltunas.inf.cu>
2019-11-25 15:40:57 -0500

    Probando ngx-export-as

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -48,6 +48,7 @@ import { SelectSubsComponent } from './pages/select-subs/select-subs.component';
 import { NbAlertModule, NbCheckboxModule} from '@nebular/theme';
 import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
 import { AuthGuard } from './services/auth-guard.service';
+import { ExportAsModule } from 'ngx-export-as';
 //import { DayOfWeekPipe } from './pipes/day-of-week.pipe';
 //import { FormatDateHumanPipe } from './pipes/format-date-human.pipe';
 //import { GetObservTextPipe } from './pipes/get-observ-text.pipe';
@@ -61,6 +62,7 @@ import { AuthGuard } from './services/auth-guard.service';
     AppComponent, UploadImgComponent, NewTaskComponent, NewObsComponent,
     LoginComponent, LogoutComponent, NewUserComponent, SelectSubsComponent],
   imports: [
+    ExportAsModule,
     NbSecurityModule,
     NbBadgeModule,
     NbListModule,
