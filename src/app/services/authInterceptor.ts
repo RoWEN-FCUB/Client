42a490163c192eb8738371c9ead3741cb3dd0f13
RoWEN-FCUB <carlos@ltunas.inf.cu>
2020-02-28 13:26:39 -0500

    arreglado que cierre sesion al expirar el token

--- src/app/services/authInterceptor.ts
+++ src/app/services/authInterceptor.ts
@@ -2,16 +2,20 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/c
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { NbAuthJWTToken, NbAuthService } from '@nebular/auth';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: NbAuthService) {}
+  constructor(private authService: NbAuthService, public router: Router) {}
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let idToken = null;
     this.authService.getToken().subscribe((token: NbAuthJWTToken) => {
       idToken = token;
     });
     if (idToken) {
+      if (!idToken.isValid()) {
+        this.router.navigate(['/auth/login']);
+      }
       const cloned = req.clone({
           headers: req.headers.set('Authorization', 'Bearer ' + idToken),
       });
