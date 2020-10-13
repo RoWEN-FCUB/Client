af09aacc1c21a94c1629748316ffb45c58290674
RoWEN-FCUB <RoWEN@RODAS-PC.ltunas.inf.cu>
2020-10-13 11:10:45 -0400

    trabajando en modulo de energia

--- src/app/pages/energy/energy.component.ts
+++ src/app/pages/energy/energy.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'energy',
   templateUrl: './energy.component.html',
-  styleUrls: ['./energy.component.css']
+  styleUrls: ['./energy.component.css'],
 })
 export class EnergyComponent implements OnInit {
 
