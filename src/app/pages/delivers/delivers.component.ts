9116feff04b1f197c8d0d8b3a2b3932c6c7bbaa0
RoWEN-FCUB <carlos@ltunas.inf.cu>
2022-04-27 10:39:39 -0400

    trabajando en actualizacion de los modulos.

--- src/app/pages/delivers/delivers.component.ts
+++ src/app/pages/delivers/delivers.component.ts
@@ -3,9 +3,10 @@ import { Observable, Subject } from 'rxjs';
 import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'delivers',
   templateUrl: './delivers.component.html',
-  styleUrls: ['./delivers.component.scss']
+  styleUrls: ['./delivers.component.scss'],
 })
 export class DeliversComponent implements OnInit {
   public webcamImage: WebcamImage = null;
@@ -16,34 +17,35 @@ export class DeliversComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    
+
   }
 
   triggerSnapshot(): void {
     this.trigger.next();
   }
- 
+
+   // tslint:disable-next-line: use-lifecycle-interface
    ngAfterViewInit() {
      // this.videoWidth = this.vale.nativeElement.getBoundingClientRect().width;
      // console.log(this.videoWidth);
      this.videoWidth = 225;
    }
- 
+
    handleImage(webcamImage: WebcamImage): void {
     // console.info('Saved webcam image', webcamImage);
     this.webcamImage = webcamImage;
    }
- 
+
    public get triggerObservable(): Observable<void> {
     return this.trigger.asObservable();
    }
- 
+
    public handleInitError(error: WebcamInitError): void {
      if (error.mediaStreamError && error.mediaStreamError.name === 'NotAllowedError') {
        console.warn('Camera access was not allowed by user!');
      }
    }
- 
+
    deleteImage(): void {
      this.webcamImage = null;
    }
